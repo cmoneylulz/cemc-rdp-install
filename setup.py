914d589fd2aae258e9f30b65ae1ef99a5d1b42bf
cmoneylulz <element4emerica@gmail.com>
2014-10-16 11:04:53 -0400

    added startup script and commands to move to correct folder for startup

--- setup.py
+++ setup.py
@@ -7,19 +7,21 @@ def run_cmd(commands, user_input = ''):
 
 #apt-get update
 commands = ['sudo', 'apt-get', 'update']
-
 run_cmd(commands)
-
 
 #apt-get upgrade
 commands[2] = 'upgrade'
 user_input = 'y'
-
 run_cmd(commands, user_input)
 
 #cp cemc.remmina ~/cemc.remmina
 commands = ['cp', 'cemc.remmina', '../cemc.remmina']
 run_cmd(commands)
 
+#cat ../cemc.remmina
 commands = ['cat', '../cemc.remmina']
+run_cmd(commands)
+
+#cp cemc-rdp.conf ../.config/upstart/cemc-rdp.conf
+commands = ['cp', 'cemc-rdp.conf', '../.config/upstart/cemc-rdp.conf']
 run_cmd(commands)
